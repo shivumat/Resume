f73409634ce76156a85d55089cd08868b11e1100
Shivank Mathur <shivumat@gmail.com>
2020-09-13 15:32:05 +0530

    Side bar partial

--- src/container/bodyPanel/BodyPanel.js
+++ src/container/bodyPanel/BodyPanel.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
 import SideBar from '../sideBar/SideBar';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    height: '100%',
   },
 }));
 
@@ -14,13 +17,15 @@ export default function BodyPanel() {
 
   return (
     <Grid container className={classes.root}>
-      <Grid item xs={12} sm={2} md={2} lg={2} xl={2}>
-        <SideBar/>
-      </Grid>
+      <Hidden xsDown>
+        <Grid item sm={3} md={2} lg={2} xl={2}>
+          <SideBar/>
+        </Grid>
+      </Hidden>
       <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
         Hello
       </Grid>
-      <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
+      <Grid item xs={12} sm={3} md={4} lg={4} xl={4}>
         Bye
       </Grid>
     </Grid>
