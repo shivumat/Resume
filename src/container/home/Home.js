e7708866f601b72439468991f4e38ad859b66dab
Shivank Mathur <shivumat@gmail.com>
2020-09-15 01:07:35 +0530

    Home Partial

--- src/container/home/Home.js
+++ src/container/home/Home.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './Home.css'
 import Grid from '@material-ui/core/Grid';
 import TitlePanel from '../titlePanel/TitlePanel';
+import About from '../about/About';
 
 export default function Home() {
 
   return (
       <Grid container className="home">
-        <Grid className={"homeTitle"} item xs={12} sm={9} md={9} lg={9} xl={9}>
+        <Grid className={"homeTitle"} item xs={12} sm={8} md={9} lg={9} xl={9}>
           <TitlePanel />
         </Grid>
-        <Grid item xs={12} sm={3} md={3} lg={3} xl={3}>
-            <div></div>
+        <Grid className={"homeAbout"} item xs={12} sm={4} md={3} lg={3} xl={3}>
+          <About />
         </Grid>
       </Grid>
   );
--- src/container/myCard/MyCard.js
+++ src/container/myCard/MyCard.js
@@ -3,9 +3,7 @@ import titleImage from '../../static/images/titleImage.jpg';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
