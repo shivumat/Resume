689c1ca25586d5604ae5fbf70590a53b5829fdeb
Shivank Mathur <shivumat@gmail.com>
2020-10-03 00:27:42 +0530

    Projects Final

--- src/container/bodyPanel/routePanels/projects/Projects.js
+++ src/container/bodyPanel/routePanels/projects/Projects.js
@@ -20,6 +20,7 @@ export default function Projects()
 
     return (
         <div className="projects">
+            {(projects !== undefined && projects.length !== 0) ? 
             <div className="projectsPanel">
                 <div className="projectsHeader">
                     Projects  
@@ -28,7 +29,16 @@ export default function Projects()
                 <div className="projectsCarouselPanel">
                     <ProjectCarousel projects={projects}/>
                 </div>
-            </div>
+            </div> :
+            <div id="notfound">
+                <div class="notfound">
+                    <div class="notfound-404">
+                        <h1>Oops!</h1>
+                        <h2>Have not done any project in {skill}</h2>
+                    </div>
+                    <a href="/Projects">Please Go Back To All Projects</a>
+                </div>
+            </div>}
         </div>
     )
 }
