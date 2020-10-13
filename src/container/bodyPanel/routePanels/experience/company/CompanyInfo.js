9cf7d4a8c950733747788198567df0cabcbd7953
Shivank Mathur <shivumat@gmail.com>
2020-10-13 23:37:42 +0530

    Experience Final

---
+++ src/container/bodyPanel/routePanels/experience/company/CompanyInfo.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import './CompanyInfo.css';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // maxWidth: 345,
+    backgroundColor : ' rgba(255, 255, 255, 0.3)'
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    width: theme.spacing(5),
+    height: theme.spacing(5),
+    backgroundColor: 'white',
+    color   :   'grey'
+  },
+  descriptionCard:{
+      paddingTop : '0'
+  }
+}));
+
+export default function CompanyInfo(props) {
+  const classes = useStyles();
+  const {index, expanded} = props;
+  const {company, companyLogo, title, startDate, endDate, location, description,alt } = props.companyInfo;
+
+  const handleExpandClick = () => {
+    props.handleExpandClick(index);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+        <Avatar variant="square" aria-label="recipe" className={classes.avatar} src={companyLogo}>{alt}</Avatar>
+        }
+        action={
+            <IconButton
+                className={clsx(classes.expand, {
+                    [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+                >
+                <ExpandMoreIcon />
+            </IconButton>
+          }
+        title={company}
+        subheader={title + '\t' + startDate + '-' + endDate + '\t' + location}
+      />
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent className={classes.descriptionCard}>
+            <Typography className="description">{description}</Typography>  
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
+
+ 
---
+++ src/static/data/experience.js
@@ -0,0 +1,32 @@
+export default [{
+    'company'       :   'Publicis Sapient',
+    'companyLogo'   :   'https://media-exp1.licdn.com/dms/image/C560BAQGBYLCVDzP_sw/company-logo_200_200/0?e=2159024400&v=beta&t=6456Nqt_YLPQ58dSlLv_MuKT1psM_0ZJgYJ2FYHUxeQ',
+    'alt'           :   'PS',
+    'title'         :   'Associate Technology',
+    'startDate'     :   'Oct\'17',
+    'endDate'       :   'Present',
+    'location'      :   'Gurgaon - India',
+    'description'   :   'Currently part of a team working on the development of banking portal for a leading hedge fund management bank of USA, using React, GWT, Java, Sybase and Oracle. Part of project for automating task tracking and alteration ( JIRA) using ChatBot, which interprets intents of the user using NLP and machine learning. Three-month training in full stack technology. The training content included HTML5, CSS3, JavaScript, React-Redux, Hibernate and Spring-Boot.'
+},
+{
+    'company'       :   'Arcelor Mittal',
+    'companyLogo'   :   'https://img.etimg.com/thumb/width-1200,height-900,imgsize-27435,resizemode-1,msid-62878627/industry/indl-goods/svs/steel/arcelormittal-ruia-backed-numetal-to-be-prime-contenders-for-essar-steel.jpg',
+    'alt'           :   'A',
+    'title'         :   'Summer Intern',
+    'startDate'     :   'May\'16',
+    'endDate'       :   'Jun\'16',
+    'location'      :   'Temirtau - Kazakhstan',
+    'description'   :   'Part of the team assigned the task to create a full working network supporting 5000 computers. Worked on deploying Active Directory policies and setting up Exchange server to start mailing services within the network. The Network system created was then used in connecting various Workshops at Arcelor Mittal Steel plant at Temirtau.'
+                        
+},
+{
+    'company'       :   'A3Logics',
+    'companyLogo'   :   'https://res-3.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1446448656/n97q8sgj35qxna7fpgvh.png',
+    'alt'           :   'A3',
+    'title'         :   'Summer Intern',
+    'startDate'     :   'May\'15',
+    'endDate'       :   'Jun\'15',
+    'location'      :   'Jaipur -  India',
+    'description'   :   'One-month training to learn the basics of JAVA application. At the end of the training submitted a JAVA applet project use for maintaining records of the patients according to their time of admission, age, type of disease, their billing history etc. Database Management was applied using SQLite. The Desktop app was then used to maintain the Hospital Records for a local Hospital.'
+}
+]
